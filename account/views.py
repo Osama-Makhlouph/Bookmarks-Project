d3661884555004ece1f8d47993e291c0bbcb64a7
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-04 13:02:38 +0200

    Added one-time messages system when editing user-profile

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 # Create your views here.
@@ -57,6 +59,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
