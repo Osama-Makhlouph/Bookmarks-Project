a3443dbf8d1c027015e810edbfe19c50993b2736
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-05 21:21:35 +0200

    added bookmarklet js-script to book mark an image, and styles the website

--- src/settings.py
+++ src/settings.py
@@ -17,7 +17,6 @@ from telnetlib import AUTHENTICATION
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 
     'social_django',
     'django_extensions',
+    'easy_thumbnails',
 
     'images.apps.ImagesConfig',
 ]
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
@@ -126,7 +123,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -142,7 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-
 
 SOCIAL_AUTH_FACEBOOK_KEY = '580366320110196'  # Facebook App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = '9b6860ead6c789b8dd69c9a210de0c9d'  # Facebook App Secret
