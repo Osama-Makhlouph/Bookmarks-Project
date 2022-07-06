7450c9c95308181ac09eaaf2f1c1113863956498
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-06 23:34:35 +0200

    Added actions-app to track users's actions

--- account/views.py
+++ account/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 
+from actions.utils import create_action
 from common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Contact, Profile
+from actions.models import Action
 # Create your views here.
 
 
@@ -36,7 +38,13 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = Action.objects.filter(user_id_in=following_ids)
+    actions = actions.select_related(
+        'user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
 
 
 def register(request):
@@ -47,6 +55,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -95,6 +104,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(
                     user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_from=request.user, user_to=user).delete()
--- src/settings.py
+++ src/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'easy_thumbnails',
 
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
 ]
 
 MIDDLEWARE = [
